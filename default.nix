98e028c1f9584bb59925c6225aeddbce76514e0e
74k1 <git@temporamail.net>
2023-11-12 13:40:06 +0100

    init. default.nix

---
+++ default.nix
@@ -0,0 +1,28 @@
+{ lib, stdenvNoCC }:
+
+stdenvNoCC.mkDerivation rec {
+  pname = "ChessSet";
+  version = "v0.1";
+
+  src = builtins.fetchurl {
+    url = "https://github.com/74k1/ChessSet/releases/download/${version}/ChessSet-${version}.otf";
+    sha256 = "sha256-1e3091e24cd544c4dc12fe9b16ce368856dd0a80aa024621374eedeb4d283348=";
+  };
+
+  installPhase = ''
+    runHook preInstall
+
+    mkdir -p $out/share/fonts/opentype
+    cp *.otf $out/share/fonts/opentype
+
+    runHook postInstall
+  '';
+
+  meta = with lib; {
+    homepage = "https://github.com/74k1/ChessSet"
+    description = "A pixelated Chess Symbols Font.";
+    license = licenses.ofl;
+    platforms = platforms.all;
+    maintainers = with maintainers; [ 74k1 ];
+  };
+}
