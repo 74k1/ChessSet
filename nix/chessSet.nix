679c5cb902dd329252dc5df4ab3ebd78eb85d90b
74k1 <git@temporamail.net>
2023-11-12 14:01:25 +0100

    feat. nix

--- nix/chessSet.nix
+++ nix/chessSet.nix
@@ -1,13 +1,15 @@
-{ lib, stdenvNoCC }:
+{ lib, inputs, outputs, pkgs, stdenvNoCC, ... }:
 
 stdenvNoCC.mkDerivation rec {
   pname = "ChessSet";
   version = "v0.1";
 
-  src = builtins.fetchurl {
-    url = "https://github.com/74k1/ChessSet/releases/download/${version}/ChessSet-${version}.otf";
-    sha256 = "sha256-1e3091e24cd544c4dc12fe9b16ce368856dd0a80aa024621374eedeb4d283348=";
-  };
+  #src = builtins.fetchurl {
+  #  url = "https://github.com/74k1/ChessSet/releases/download/${version}/ChessSet-${version}.otf";
+  #  sha256 = "sha256-1e3091e24cd544c4dc12fe9b16ce368856dd0a80aa024621374eedeb4d283348=";
+  #};
+
+  src = ./..;
 
   installPhase = ''
     runHook preInstall
@@ -19,7 +21,7 @@ stdenvNoCC.mkDerivation rec {
   '';
 
   meta = with lib; {
-    homepage = "https://github.com/74k1/ChessSet"
+    homepage = "https://github.com/74k1/ChessSet";
     description = "A pixelated Chess Symbols Font.";
     license = licenses.ofl;
     platforms = platforms.all;
